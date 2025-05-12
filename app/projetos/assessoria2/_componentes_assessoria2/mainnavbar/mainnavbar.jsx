5ffba2f123b0595814b6587e4e3545fed2a8ed87
Anderson23A <andersonassis023@hotmail.com>
2025-05-12 16:12:43 -0300

    update

--- app/projetos/assessoria2/_componentes_assessoria2/mainnavbar/mainnavbar.jsx
+++ app/projetos/assessoria2/_componentes_assessoria2/mainnavbar/mainnavbar.jsx
@@ -7,60 +7,7 @@ import Link from "next/link";
 import "./mainnavbar.scss";
 
 export default function MainNavbar() {
-  const [isDesktop, setIsDesktop] = useState(
-    window.matchMedia("(min-width: 992px)").matches
-  );
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(min-width: 992px)");
-    const handleResize = () => setIsDesktop(mediaQuery.matches);
-
-    handleResize();
-    mediaQuery.addEventListener("change", handleResize);
-
-    return () => mediaQuery.removeEventListener("change", handleResize);
-  }, []);
-
-  return isDesktop ? (
-    <Navbar expand="lg" sticky="top" className="navbar-custom">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} href="#" className="nav-link">
-              Início
-            </Nav.Link>
-            <Nav.Link as={Link} href="#about" className="nav-link">
-              Sobre Nós
-            </Nav.Link>
-            <Nav.Link as={Link} href="#services" className="nav-link">
-              Serviços
-            </Nav.Link>
-          </Nav>
-          <Navbar.Brand className="logo-container" href="#home">
-            <Image
-              src={logoIMG}
-              alt="Logo Alvo Estratégico"
-              className="logo"
-              width={100}
-              height={100}
-            />
-          </Navbar.Brand>
-          <Nav>
-            <Nav.Link as={Link} href="#portfolio" className="nav-link">
-              Portfólio
-            </Nav.Link>
-            <Nav.Link as={Link} href="#contact" className="nav-link">
-              Contato
-            </Nav.Link>
-            <Button variant="warning" className="cta-btn">
-              Solicitar Proposta
-            </Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  ) : (
+  return (
     <Navbar expand="lg" sticky="top" className="navbar-custom">
       <Container className="">
         <Navbar.Brand className="logo-container" href="#home">
